2ef99e2426e9e01f768752dfc3c980f21d973b7b
jayho <jay@one45.com>
2020-05-01 21:59:29 -0700

    Reordered files

---
+++ src/components/SaleCardContainer/SaleCardContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import NormalSaleCard from "../NormalSaleCard/NormalSaleCard";
+
+function SaleCardContainer(props) {
+  return (
+      <NormalSaleCard/>
+  );
+}
+
+export default SaleCardContainer;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import NormalSaleCard from "./NormalSaleCard/NormalSaleCard";
+import NormalSaleCard from "./components/NormalSaleCard/NormalSaleCard";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
