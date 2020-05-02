a315f0bdb357b47a7908f0528864200d560cb314
Kevin Peng <kevinpkf311@gmail.com>
2020-05-02 16:47:21 -0700

    Added 'select-all' and 'select-none' functionality

--- src/components/AdvancedSearch/AdvancedSearch.js
+++ src/components/AdvancedSearch/AdvancedSearch.js
@@ -13,6 +13,8 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const useStyles = makeStyles({
     box: {
@@ -21,45 +23,68 @@ const useStyles = makeStyles({
         paddingBottom: 12,
         paddingLeft: 10,
         paddingRight: 10,
-        boxShadow: '1px 3px 3px 1px rgba(200, 200, 255, .2)'
+        marginLeft: 1,
+        backgroundColor: 'background.paper',
+        borderRadius: 4,
+        border: "1px solid #DDE3E6",
+        boxShadow: '1px 1px 2px  rgba(150, 150, 250, 0.15)'
     },
     title: {
         fontSize: 20,
-        textAlign: 'center'
+        textAlign: 'center',
+        color: '#1B1B1E',
     },
     expansionHeading: {
-        fontSize: 14,
+        fontSize: 16,
     },
-    expansionBar: {
-        
+    subHeading: {
+        fontSize: 13,
+        color: '#323235',
+        marginTop: 5,
     },
     scrollDiv: {
         overflowY: "auto",
         maxWidth: 200,
         width: "fit-content",
-        height: 250,
+        height: 300,
     },
     formControl: {
         margin: 8,
         minWidth: 190,
-    }
+    },
+    expansionPanel: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    selectToggle: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginBottom: 12,
+    },
+    buttonGroup: {
+        margin: 'auto',
+    },
   });
 
 function AdvancedSearch(props) {
     const classes = useStyles();
 
-    //Hardcoded companyList for now, but later should be routed from backend of a list of supported companies
+    //Hardcoded companyList for now, but later should be routed from backend as a list of supported companies
     const companyList = ['A&F', 'Adidas', 'Aritzia', 'Banana Republic', 'Frank and Oak', 'Gap', 'H&M', 'Hollister'];
 
-    //using the array of supported companies, create the state object to pass to React.useState(-);
-    var companyObject = {}
+    //using the array of supported companies, create the state object to pass to React.useState(...);
+    //also create a state object with all values to be the compliment of the initial state
+    var companyObjectTrue = {}
+    var companyObjectFalse = {}
     for(var i = 0; i < companyList.length; i++){
-        companyObject[companyList[i]] = true
+        companyObjectTrue[companyList[i]] = true
+        companyObjectFalse[companyList[i]] = false
     }
 
     //useState Hooks START
     //useState for company filter object... ex. {nike: true, adidas: false, ...}
-    const [companyFilter, setCompanyFilter] = React.useState(companyObject);
+    const [companyFilter, setCompanyFilter] = React.useState(companyObjectTrue);
 
     //useState for sort type from selection dropdown... ex. 'Recent', 'Rating - High to Low, etc.
     const [sortType, setSortType] = React.useState('Recent');
@@ -73,7 +98,17 @@ function AdvancedSearch(props) {
     //change handler for companyFilter: sets the company boolean using the Checkbox change event
     const handleChangeCompanyFilter = (event) => {
         setCompanyFilter({ ...companyFilter, [event.target.name]: event.target.checked });
-      };
+    };
+
+    //handleSelectAll changes state of all items in companyFilter to true
+    const handleSelectAll = () => {
+        setCompanyFilter(companyObjectTrue);
+    }
+
+    //handleSelectNone changes state of all items in companyFilter to false
+    const handleSelectNone = () => {
+        setCompanyFilter(companyObjectFalse);
+    }
 
     //Create JSX elements in a companyFilterList array to insert into the React return statement for the filter list part of the component
     const companyFilterList = companyList.map((company) => {
@@ -90,7 +125,7 @@ function AdvancedSearch(props) {
 
     //Start React return
     return(
-        <Box className={classes.box} borderRadius={8} bgcolor="#FBF8FC">
+        <Box className={classes.box}>
             <Typography className={classes.title}>
                 Advanced Search
             </Typography>
@@ -121,8 +156,17 @@ function AdvancedSearch(props) {
                     >
                         <Typography className={classes.expansionHeading}>Filter Companies</Typography>
                     </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                            <div className={classes.scrollDiv}>
+                    <ExpansionPanelDetails className={classes.expansionPanel}>
+                        <div className = {classes.selectToggle}>
+                            <Typography className={classes.subHeading}>
+                                Select:
+                            </Typography>
+                            <ButtonGroup size="small" aria-label="select toggle" className={classes.buttonGroup}>
+                                <Button onClick={handleSelectAll}>All</Button>
+                                <Button onClick={handleSelectNone}>None</Button>
+                            </ButtonGroup>
+                        </div>
+                        <div className={classes.scrollDiv}>
                             {companyFilterList}
                         </div>
                     </ExpansionPanelDetails>
