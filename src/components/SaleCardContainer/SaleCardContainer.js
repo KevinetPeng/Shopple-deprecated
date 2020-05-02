b6a8fc559fdface7bf7a054cf95cb6e1a95e9d2b
jayho <jay@one45.com>
2020-05-01 22:40:37 -0700

    created card container

--- src/components/SaleCardContainer/SaleCardContainer.js
+++ src/components/SaleCardContainer/SaleCardContainer.js
@@ -1,13 +1,18 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
 import NormalSaleCard from "../NormalSaleCard/NormalSaleCard";
+import "./SaleCardContainer.css";
 
 function SaleCardContainer(props) {
-  return (
-      <NormalSaleCard/>
-  );
+  const sales = props.sales;
+  const listItems = sales.map((element) => (
+    <NormalSaleCard
+      companyName="H&M"
+      saleDescript="
+  Support your community and broaden your horizons by trying different restaurants in your neighbourhood. Get inspired by the cuisines highlighted below, and experience flavours from around the world in the comfort of your home* with a $0 Delivery Fee on orders of $20 or more."
+      saleAmount="20% Off"
+    />
+  ));
+  return <ul className="sale-card-container">{listItems}</ul>;
 }
 
 export default SaleCardContainer;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import NormalSaleCard from "./components/NormalSaleCard/NormalSaleCard";
 import * as serviceWorker from "./serviceWorker";
+import SaleCardContainer from "./components/SaleCardContainer/SaleCardContainer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <NormalSaleCard
-      companyName="H&M"
-      saleDescript="
-      Support your community and broaden your horizons by trying different restaurants in your neighbourhood. Get inspired by the cuisines highlighted below, and experience flavours from around the world in the comfort of your home* with a $0 Delivery Fee on orders of $20 or more."
-      saleAmount="20% Off"
-    />
+    <SaleCardContainer sales={[1, 2, 3, 4, 5]} />
   </React.StrictMode>,
   document.getElementById("root")
 );
