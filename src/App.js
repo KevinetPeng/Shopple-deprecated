662ee9533a3faf5254bb165388a46745d79d736a
jayho <jay@one45.com>
2020-05-04 00:13:16 -0700

    Combined components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
-import AppBar from "./components/AppBar";
+import AppBar from "./components/AppBar/AppBar";
 import SaleCardContainer from "./components/SaleCardContainer/SaleCardContainer";
+import AdvancedSearch from "./components/AdvancedSearch/AdvancedSearch";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <AppBar />
-      <SaleCardContainer sales={[1, 2, 3, 4, 5]} />
+      <div className="App">
+        <AdvancedSearch />
+        <SaleCardContainer sales={[1, 2, 3, 4, 5]} />
+      </div>
     </div>
   );
 }
