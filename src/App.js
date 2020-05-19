4a16db3dc9654f394f9490a158b33a7a442ef7a9
Kevin Peng <kevinpkf311@gmail.com>
2020-05-18 23:41:14 -0700

    Implemented list-view and card-view toggle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SaleCardContainer from "./components/SaleCardContainer/SaleCardContainer"
 import AdvancedSearch from "./components/AdvancedSearch/AdvancedSearch";
 import AddSale from "./components/AddSale/AddSale";
 import ToggleView from "./components/ToggleView/ToggleView";
+import SaleListContainer from "./components/SaleListContainer/SaleListContainer";
 
 function App(props) {
   const [showListView, setShowListView] = React.useState(true);
@@ -20,7 +21,29 @@ function App(props) {
         <div className="app">
           <AdvancedSearch />
           <div className="space-left" />
-          <SaleCardContainer sales={[1, 2, 3, 4, 5]} />
+          {!showListView && <SaleCardContainer sales={[1, 2, 3, 4, 5]} />}
+          {showListView && (
+            <SaleListContainer
+              sales={[
+                1,
+                2,
+                3,
+                4,
+                5,
+                "sale",
+                "cool",
+                true,
+                7,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+              ]}
+            />
+          )}
           <div className="space-left" />
           <ToggleView parentCallback={toggleViewCallback} />
           <div className="space-right" />
